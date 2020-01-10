612b9b3fcb8d4412a404d8a40d73f13bfb111373
zzj <863438297@qq.com>
2020-01-10 17:19:59 +0800

    static和类加载

---
+++ Base/Static(ClassLoad)/src/main/java/com/code/sta/StaticMethod.java
@@ -0,0 +1,11 @@
+package com.code.sta;
+
+/**
+ * @ClassName StaticMethod
+ * @Description 静态方法的类加载（类初始化的时候并不会执行静态方法）
+ * @Author zzj
+ * @Date 2020/1/10
+ * @Version v1.0
+ **/
+public class StaticMethod {
+}
